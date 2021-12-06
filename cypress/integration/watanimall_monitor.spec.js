d03805b31a671c4b5fd1f65096e373abcfaf1802
nesma <nesmazuhier@gmail.com>
2021-12-06 10:49:44 +0200

    clicking on all categories menu item tests

--- cypress/integration/watanimall_monitor.spec.js
+++ cypress/integration/watanimall_monitor.spec.js
@@ -3,4 +3,15 @@ describe("Add monitor to cart in Watanimall",()=>{
     before(()=>{
         cy.visit("/")
     })
+    it("Verify Hovering on 'all categories' menu item",()=>{
+        cy.get('li[id="menu-item-104788"]').trigger('mouseover')
+        // cy.get('li[id="menu-item-104788"]').should('have.class','hover')
+    })
+    it("Verify clicking on 'all categories' menu item",()=>{
+        cy.get('li[id="menu-item-104788"]').click()
+        cy.get('li[id="menu-item-104788"]').should('have.class','current-menu-item')
+    })
+    it("Verify clicking on 'all categories' menu item redirect the user to all categories page",()=>{
+        cy.url().should('include','all-categories')
+    })
 })
